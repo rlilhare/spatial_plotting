c32c1eae91dc85bc305d2b5a3be9759eab500efe
RAJTANTRA LILHARE <lilhare.rajtantra560@gmail.com>
2021-06-04 16:01:50 -0700

    Formatting
    required formatting done

--- src/ncfile_read_store.py
+++ src/ncfile_read_store.py
@@ -1,12 +1,16 @@
-from netCDF4 import Dataset as NetCDFFile 
 import os
+
+from netCDF4 import Dataset as NetCDFFile
 
 
 def read_nc_data():
     base_path = os.getcwd()
-    air_temp_path = os.path.join(base_path, './data/ERA_Land_mn_ann_air_temp_NRB_1981_2019.nc')
-    nc = NetCDFFile(air_temp_path) # Input your file in a netcdf format
-    lat = nc.variables['lat'][:]
-    lon = nc.variables['lon'][:]
-    temp = nc.variables['temp'][:] # We are plotting this variable in the current plot (avg ann air temperature)
+    air_temp_path = os.path.join(
+        base_path, "./data/ERA_Land_mn_ann_air_temp_NRB_1981_2019.nc"
+    )
+    nc = NetCDFFile(air_temp_path)  # Input your file in a netcdf format
+    lat = nc.variables["lat"][:]
+    lon = nc.variables["lon"][:]
+    # Plotting variable in the plot (avg ann air temp)
+    temp = nc.variables["temp"][:]
     return lat, lon, temp
